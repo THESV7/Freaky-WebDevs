389d16272825373dce6c9bdd02c0f950c5f54886
THESV7 <vadhelsmit@gmail.com>
2023-10-07 08:28:30 +0530

    Home UI Update

--- client/src/Components/Home/components/CoursesList.jsx
+++ client/src/Components/Home/components/CoursesList.jsx
@@ -9,10 +9,11 @@ const CoursesList = () => {
                     <div className='CoursesList_content'>
                         <p className="CoursesList_title">Available Courses</p>
                         <h1 className="Courses_short_description">
-                            Welcome to our e-learning platform for prisoners. Explore the courses below to enhance your knowledge and skills.
+                        Welcome to our e-learning platform for prisoners. Explore and enhance your skills.
+
                         </h1>
                         <p className="Courses_description">
-                            Our courses cover a wide range of topics to help you grow and learn during your time in prison.
+                        Discover a variety of courses to support your growth and learning during your time in prison.
                         </p>
                     </div>
                     <div className="CoursesList_card_container">
--- client/src/Components/Home/components/PlatformServices.jsx
+++ client/src/Components/Home/components/PlatformServices.jsx
@@ -1,59 +1,59 @@
 import React from 'react'
 import './PlatformServices.css'
-import coursesIcon from '../../../assets/courses.png'
-import activitiesIcon from '../../../assets/activites.png' 
-import videoIcon from '../../../assets/video_conferernce.png' 
+// import coursesIcon from '../../../assets/courses.png'
+// import activitiesIcon from '../../../assets/activites.png' 
+// import videoIcon from '../../../assets/video_conferernce.png' 
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook, faGamepad, faVideo } from '@fortawesome/free-solid-svg-icons';
+
 const PlatformServices = () => {
     const serviceData = [
         {
             title: 'Courses',
             description: 'Courses for prisoners to acquire new skills and knowledge.',
-            icon: coursesIcon, 
+            icon: faBook, // FontAwesome icon
         },
         {
             title: 'Activities for Prisoners',
             description: 'Activities to engage and support prisoners.',
-            icon: activitiesIcon, 
+            icon: faGamepad, // FontAwesome icon
         },
         {
             title: 'Video Conferencing',
-            description: 'Secure video conferencing services for live communicate with mentor',
-            icon: videoIcon, 
+            description: 'Secure video conferencing services for live communication with mentors.',
+            icon: faVideo, // FontAwesome icon
         }
     ];
-    
+
     return (
-        <>
-            <section className='Service_section'>
-                <div className="service_container">
-                    <div className="service_title">
-                        <span>Our Services</span>
-                    </div>
-                    <div className="service_description">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    </div>
-                    <div className="service_cards">
-                        {
-                            serviceData.map(({icon, title , description}) => (
-                                <div className="service_card">
-                                    <div className="service_icon_title">
-                                        <img src={icon} alt="" />
-                                        <span>{title}</span>
-                                    </div>
-                                    <div className="service_card_description">
-                                        <p>{description}</p>
-                                    </div>
-                                    <div className='service_card_btn'>
-                                        <button>Learn More</button>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                    </div>
+        <section className='Service_section'>
+            <div className="service_container">
+                <div className="service_title">
+                    <span>Our Services</span>
                 </div>
-            </section>
-        </>
-    )
+                <div className="service_description">
+                    Here are some of the services we offer:
+                </div>
+                <div className="service_cards">
+                    {serviceData.map(({ icon, title, description }, index) => (
+                        <div className="service_card" key={index}>
+                            <div className="service_icon_title">
+                                <FontAwesomeIcon icon={icon} size="3x" /> {/* Use FontAwesome icon */}
+                                <span>{title}</span>
+                            </div>
+                            <div className="service_card_description">
+                                <p>{description}</p>
+                            </div>
+                            <div className='service_card_btn'>
+                                <button>Learn More</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
 
-export default PlatformServices
+export default PlatformServices;
